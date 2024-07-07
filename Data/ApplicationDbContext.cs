09243b51b6dc6848484cdcb4fe0416f2d6042f24
Rodrigo Garcia <160806289+Roykungfu@users.noreply.github.com>
2024-07-07 18:02:28 -0300

    Add files via upload

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Data
+{
+    public class ApplicationDbContext : DbContext // DbContext esta dentro de las clases de EntityFramework
+
+	// DbContext es una clase en Entity Framework, un popular Object-Relational Mapper (ORM) para .NET.
+    // DbContext actúa como un puente entre tu aplicación y la base de datos, permitiéndote interactuar con la base de datos utilizando objetos
+    // y clases de C# en lugar de comandos SQL directamente.
+
+
+    
+	{
+           
+        public ApplicationDbContext() : base() { }   // Este es el constructor, se puede crear rapidamente con las siglas "ctor" + TAB
+        {
+            
+            public static string ConnectionString   { get; set; }
+
+		protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+		{
+			// base.OnConfiguring(optionsBuilder); Esto no hace falta
+
+
+
+
+		}
+
+
+	}
+
+        
+
+
+    }
+}
---
+++ Data/InyeccionDependencias.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data
+{
+
+	// la inyección de dependencias (Dependency Injection, DI) es un patrón de diseño que facilita la creación de software modular, flexible y fácil de mantener.
+	// La idea básica es que una clase (dependiente) no debe crear sus propias dependencias (otros objetos de los que depende), sino que estas dependencias deben
+	// ser proporcionadas (inyectadas) por un contenedor de inyección de dependencias. Esto ayuda a reducir el acoplamiento entre las clases y mejora la testabilidad.
+
+
+
+	// Ejemplo sin inyeccion de dependencias
+
+	public class Engine
+	{
+		public void Start()
+		{ Console.WriteLine("Engine started"); }	}
+
+	public class Car
+	{	private Engine _engine;
+		public Car()
+			{ _engine = new Engine(); }// La clase Car crea su propia instancia de Engine
+
+		public void Start()
+		{	_engine.Start();
+			Console.WriteLine("Car started");}
+
+
+		/////// Con inyeccion de dependencias/////
+
+		public class Engine
+		{
+			public void Start()
+			{
+				Console.WriteLine("Engine started");
+			}
+		}
+
+		public class Car
+		{
+			private Engine _engine;
+
+			// El motor es inyectado a través del constructor
+			public Car(Engine engine)
+			{
+				_engine = engine;
+			}
+
+			public void Start()
+			{
+				_engine.Start();
+				Console.WriteLine("Car started");
+			}
+		}
+
+
+
+
+	}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
+
+
